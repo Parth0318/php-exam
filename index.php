0e66de6626ad7636c129eb1d58c8eda8b0599dca
parth <parth.rohit1812@gmail.com>
2023-10-11 00:25:50 +0530

    ti commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+  include("session.php");
+  $exp_category_dc = mysqli_query($con, "SELECT expensecategory FROM expenses WHERE user_id = '$userid' GROUP BY expensecategory");
+  $exp_amt_dc = mysqli_query($con, "SELECT SUM(expense) FROM expenses WHERE user_id = '$userid' GROUP BY expensecategory");
+
+  $exp_date_line = mysqli_query($con, "SELECT expensedate FROM expenses WHERE user_id = '$userid' GROUP BY expensedate");
+  $exp_amt_line = mysqli_query($con, "SELECT SUM(expense) FROM expenses WHERE user_id = '$userid' GROUP BY expensedate");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Feather JS for Icons -->
+  <script src="js/feather.min.js"></script>
+  <style>
+    .card a {
+      color: #000;
+      font-weight: 500;
+    }
+
+    .card a:hover {
+      color: #28a745;
+      text-decoration: dotted;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="border-right" id="sidebar-wrapper">
+      <div class="user">
+        <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="120">
+        <h5><?php echo $username ?></h5>
+        <p><?php echo $useremail ?></p>
+      </div>
+      <div class="sidebar-heading">Management</div>
+      <div class="list-group list-group-flush">
+        
+        <a href="add_expense.php" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
+        <a href="manage_expense.php" class="list-group-item list-group-item-action "><span data-feather="dollar-sign"></span> Manage Expenses</a>
+      </div>
+      <div class="sidebar-heading">Settings </div>
+      <div class="list-group list-group-flush">
+       
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg navbar-light  border-bottom">
+
+
+        <button class="toggler" type="button" id="menu-toggle" aria-expanded="false">
+          <span data-feather="menu"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="25">
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+               
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+       
+        <div class="row">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col text-center">
+                    <a href="add_expense.php"><img src="icon/addex.png" width="57px" />
+                      <p>Add Expenses</p>
+                    </a>
+                  </div>
+                  <div class="col text-center">
+                    <a href="manage_expense.php"><img src="icon/maex.png" width="57px" />
+                      <p>Manage Expenses</p>
+                    </a>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div class="row">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-header">
+                
+              </div>
+              <div class="card-body">
+                <canvas id="expense_line" height="150"></canvas>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title text-center">Expense Category</h5>
+              </div>
+              <div class="card-body">
+                <canvas id="expense_category_pie" height="150"></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  
+  
+</body>
+
+</html>
